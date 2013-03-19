89d1ffecdf55cfca68d4a4f7a1ae6714f71a3cdf
TsungWei Hu <marr.tw@gmail.com>
2013-03-19 11:29:09 +0800

    Update Browser View for Folder type. Update Template for Article type.

--- tbfac/content/folder.py
+++ tbfac/content/folder.py
@@ -42,6 +42,12 @@ class View(grok.View):
         if membership.isAnonymousUser():
             self.request.set('disable_border', True)
 
+    def toLocalizedTime(self, time, long_format=None, time_only=None):
+        """Convert time to localized time
+        """
+        util = getToolByName(self.context, 'translation_service')
+        return util.ulocalized_time(time, long_format, time_only, self.context, domain='plonelocales')
+
     def getItems(self):
         """ Get Items to Display
         """
