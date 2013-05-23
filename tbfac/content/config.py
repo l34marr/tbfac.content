5153179526d22b84b505f4c84b992fcda211409f
Vitaliy Podoba <vitaliypodoba@gmail.com>
2013-05-23 11:18:24 +0300

    add Most Liked portlet working via addthis.com

---
+++ tbfac/content/config.py
@@ -0,0 +1,8 @@
+# addthis.com settings
+ADDTHIS_DOMAIN = 'talks.taishinart.org.tw'
+ADDTHIS_PUBID = 'ra-50c89b7d2a700f0f'
+ADDTHIS_PERIOD = 'month'
+ADDTHIS_USERNAME = 'email@username.com'
+ADDTHIS_PASSWORD = '******'
+# 6 hours to cache addthis.com data, in seconds
+ADDTHIS_CACHE_TIME = 6 * 60 * 60
