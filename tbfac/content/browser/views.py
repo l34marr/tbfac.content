392cb8f2c0a6e4b6289bb9b5f1d143ebded803ab
TsungWei Hu <marr.tw@gmail.com>
2013-12-20 17:19:57 +0800

    Add Visit Count Browser View and Template.

--- tbfac/content/browser/views.py
+++ tbfac/content/browser/views.py
@@ -3,6 +3,12 @@ from zope.component import getUtility
 
 from Products.Five.browser import BrowserView
 
+from Products.CMFCore.utils import getToolByName
+import urllib2
+import json
+from ..config import ADDTHIS_API_ADD, ADDTHIS_USERNAME, ADDTHIS_PASSWORD
+from ..config import ONETIME_FILE
+
 from plone.supermodel.model import Schema
 try:
     from plone.directives.form.schema import Schema as oldSchema
@@ -40,3 +46,50 @@ class FixCatalogView(BrowserView):
                     if oldSchema in v:
                         v.remove(oldSchema)
                         v.add(Schema)
+
+
+class AddThisAdd(BrowserView):
+    def __call__(self):
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        # Add the username and password.
+        password_mgr.add_password(None, ADDTHIS_API_ADD, ADDTHIS_USERNAME, ADDTHIS_PASSWORD)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        # create "opener" (OpenerDirector instance)
+        opener = urllib2.build_opener(handler)
+        # use the opener to fetch a URL
+        resultJson = opener.open(api_url)
+        # read result to to string format
+        resultStr = resultJson.read()
+        # load string to list format
+        resultList = json.loads(resultStr)
+
+        for record in resultList:
+            try:
+                path = str(record['url'].split('?')[0].split('/view')[0].replace('http://talks.taishinart.org.tw', '/taishin'))
+                catalog = getToolByName(self, 'portal_catalog')
+                brains = catalog(path=path)
+                item = brains[0].getObject()
+                prev_counts = getattr(item, 'v_counts', 0)
+                item.v_counts = prev_counts
+                item.v_counts += int(record['clicks'])
+            except: pass
+
+
+class OneTime(BrowserView):
+    def __call__(self):
+        catalog = getToolByName(self, 'portal_catalog')
+        with open(ONETIME_FILE, 'r') as onetime:
+            for line in onetime.read().split('\n'):
+                if line == '':
+                    break
+                url, shares, clicks = str(line.split('\t')[0]), int(line.split('\t')[1]), int(line.split('\t')[2])
+                path = url.replace('http://talks.taishinart.org.tw', '/taishin').split('?')[0]
+                if path.split('/')[2] not in ['event', 'juries']:
+                    continue
+                brain = catalog(path=path)
+                item = brain[0].getObject()
+                prev_counts = getattr(item, 'v_counts', 0)
+                item.v_counts = prev_counts
+                item.v_counts += shares
+                item.v_counts += clicks
+
